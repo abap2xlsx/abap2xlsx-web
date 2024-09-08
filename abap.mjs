aea1bff421e06b0b6e553dc999cb15e617ebf62d
Lars Hvam <larshp@hotmail.com>
2024-09-08 10:36:34 +0200

    update examples

--- abap.mjs
+++ abap.mjs
@@ -1,7 +1,7 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
 
-console.log("Building abap.js");
+console.log("Running abap.mjs");
 
 let output = "export const abapfiles = {\n";
 let files = {};
